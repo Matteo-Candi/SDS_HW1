f86fd74172c2c49030937b03c81ff2f899bc6c46
PaoloBarba <114430394+PaoloBarba@users.noreply.github.com>
2022-11-20 19:22:38 +0100

    comment on the first question

--- Homework.r
+++ Homework.r
@@ -62,6 +62,23 @@ grid()
 
 #Comments:
 
+#Supponiamo di  dover pagare una cifra c per giocare a questo gioco e si vincono tot euro quanto è il tempo di attesa.Ci si può chiedere qual'è la cifra "giusta" per partecipare a questo gioco.
+
+#In media ci si aspetta di vincere una somma infinita da questo gioco,quindi, in accordo con la teoria tradizionale del valore atteso,ci si può permettere di pagare qualunque cifra c per partecipare. Infatti anche pagando un miliardo per volta, alla lunga, dovrà capitare la volta (con probabilità invero bassissima) di una vincita così strepitosa da ripagare abbondantemente tutte le altre quote investite per ottenere vincite insignificanti.In pratica però, nessuna persona ragionevole è disposta a pagare più di qualche unità per partecipare a questo gioco. Il rifiuto intuitivo a investire cifre importanti nel gioco è ben sostenuto dalla simulazione descritta nel grafico seguente. Ripetendo 10000 volte la serie si ottengono all'inizio vincite mediamente basse (qualche unità). Successivamente la media si alza, in corrispondenza di qualche serie fortunata, per poi diminuire leggermente fino al prossimo colpo fortunato. L'andamento complessivo è senza dubbio crescente, e tenderà matematicamente all'infinito dopo una serie infinita di giocate ma, nelle 10.000 giocate della simulazione, la media delle vincite è arrivata appena al valore di 6.
+
+
+#Dal punto di vista matematico, non sorgono difficoltà dalla situazione prospettata nel gioco.In altre parole, è perfettamente coerente accettare la possibilità (infinitesima) di una vincita infinita, tale da bilanciare qualunque somma pagata nelle (infinite) volte in cui la vincita risulta insignificante.
+
+#Possibile soluzione, vincita massima fissata a L:
+
+
+
+
+
+
+
+
+
 
 
 
