e5b1b6cdfc41caef55539f7f489c5f17702c8ef8
PaoloBarba <114430394+PaoloBarba@users.noreply.github.com>
2022-11-20 17:00:30 +0100

    Adding first exercise

--- Homework.r
+++ Homework.r
@@ -1,41 +1,71 @@
+# Homework 1 of Statistcs for Data Science
+# Accademic year: 2022/2023
+# Authors: Barba Paolo, Candi Matteo
+rm(list=ls())
+
+# Exercise 1 Stopping time   ----------------------------- 
+######### Brutti's code############################
+M <- 1000             # simulation size
+t_data <- rep(NA, M)   # pre-alloc the data structure (simple vector)
+for (m in 1:M){
+  x <- runif(1)    # sampling the threshold value
+  y <- runif(1)    # first process value
+  t <- 1
+  while (y > x) {
+    t <- t + 1
+    y <- runif(1)  # no memory
+  }
+  t_data[m] <- t   # save
+}
+p_hat <- proportions( table( t_data ) )
 
 
-# Exercise rush -----------------------------------------------------------
+# True marginal PMF of T
+pT <- function(t)  1/(t*(t+1))
+
+# Plot it zooming in between t = 1 and t = 25
+t_seq <- 1:25
+
+# True PMF of T
+plot(t_seq, pT(t_seq), 
+     type = "h",
+     lwd = 4,
+     col = "cyan4",
+     ylab = expression(p[T]),
+     xlab = "t",
+     main = "Marginal of the Stopping Time",
+     sub = paste("Simulation size:", M))
+
+points(t_seq, pT(t_seq), pch = 21, 
+       col = "black", bg = "darkgoldenrod2", cex = 1.5)
+
+# Simulative approximation to the PMF of T
+points(t_seq, p_hat[t_seq], pch = 24, 
+       col = "black", bg = "yellow", cex = .7)
 
 
-M <- c(100, 1000, 10000, 100000, 1000000, 10000000)
-set.seed(123)
-l=c()
-for(m in 1:100){
-  u <- runif(1)
-  set.seed(123)
-  g <- rgeom(1,u)
-  l=c(l,g)
+step_size = 10
+ave_step  = seq(1, length(t_data), step_size)
+ave_vec = rep(NA, length(ave_step))
 
-  
+t <- 0
+for (i in ave_step){
+  t <- t + 1
+  ave_vec[t] = mean( t_data[1:i] )
 }
 
-hist(l , breaks = 1000 , xlim = c(0,10))
 
-set.seed(123)
-m <-  100
-k <- runif(m)
-g <- rgeom(m , u)
+plot(ave_vec , main = "Average stopping time",
+     xlab = "step", ylam = "Averege stopping time",
+     sub = "Simulation study", col = "steelblue" , type = "l" , lwd =3)
+grid()
 
-hist(g , breaks = 10 , xlim = c(0,10), freq = F)
-points(1:10, dgeom(1:10, prob = 0) , add = T , lwd = 3)
+#Comments:
 
 
 
 
 
-# 2) The Exercise ---------------------------------------------------------
-
-curve(dbeta(x,10,10))
-
-n <- c(100, 1000)
-eps <- c(.1, .001)
-M <- 5:50
 
 
 
